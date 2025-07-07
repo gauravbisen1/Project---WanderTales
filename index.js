b2be467bffa05820ee0a92a5abd5607abbea5f6c
gauravbisen1 <gouravbisen123@gmail.com>
2025-07-07 09:10:04 +0530

    fetched data from DB!

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
 });
 
-//api
 app.get("/", (req,res)=>{
     res.send("Root !");
 });
@@ -22,3 +26,17 @@ main().then(()=>{
 }).catch(err =>{
     console.log(err);
 });
+
+//index route
+app.get("/listings", async(req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
+
+//show route
+app.get("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+})
+
