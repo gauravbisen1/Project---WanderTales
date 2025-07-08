2e3bf2a3aae6250427dd8d9c5a60345161e9b431
gauravbisen1 <gouravbisen123@gmail.com>
2025-07-08 13:13:49 +0530

    starting with frontend

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
+app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
